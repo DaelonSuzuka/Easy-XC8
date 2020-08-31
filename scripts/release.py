db3b90f9ee422292ac5a4774081d534c7b8715bc
DaelonSuzuka <david@ldgelectronics.com>
2020-08-31 16:28:33 -0400

    Release target works now

--- scripts/release.py
+++ scripts/release.py
@@ -4,27 +4,31 @@ import os
 import sys
 import yaml
 from dotmap import DotMap
-from pathlib import Path
 from build import xc8, xc8_cc
 
 
-
 def main(project):
-    if project.compiler == 'xc8-cc':
-        command = xc8_cc(project)
-    else:
-        command = xc8(project)
+    for processor in project.release_processors:
+        project.processor = processor
 
-    try:
-        result = os.system(command)
-    except KeyboardInterrupt:
-        result = 1
-    sys.exit(result)
+        if project.compiler == 'xc8-cc':
+            command = xc8_cc(project)
+        else:
+            command = xc8(project)
+
+        try:
+            os.system(command)
+        except KeyboardInterrupt:
+            sys.exit(1)
+        
+        hexfile = f"{project.build_dir}/{project.name}"
+
+        os.rename(f'{hexfile}.hex', f'{hexfile}_{project.processor}.hex')
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
     project = DotMap(yaml.full_load(open("project.yaml").read()))
-
-    print(project.release_processors)
 
     main(project)
