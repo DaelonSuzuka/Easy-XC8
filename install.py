bd8340d08c6b92ffc0adfb0f33ed4129d0584f4d
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-11-13 02:50:22 -0500

    Serious improvements to install.py

--- install.py
+++ install.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
+
+import os
 from pathlib import Path
 import shutil
 
-project_path = Path(__file__).absolute().parent.parent
+
 templates_folder = Path(Path(__file__).absolute().parent, 'templates')
 
-makefile_path = Path(project_path, "Makefile")
-if not makefile_path.exists():
-    shutil.copyfile(Path(templates_folder, 'Makefile'), makefile_path)
+
+def copy_file(filename, dest):
+    if d := os.path.dirname(dest):
+        os.makedirs(d, exist_ok=True)
+    shutil.copyfile(Path(templates_folder, filename), Path(dest))
+
+
+files = {
+    'Makefile': 'Makefile',
+    'c_cpp_properties.json': '.vscode/c_cpp_properties.json',
+    'cogfiles.txt': 'cogfiles.txt',
+    'pins.csv': 'pins.csv',
+    'pins.c': 'src/pins.c',
+    'pins.h': 'src/pins.h',
+    '.clang-format': '.clang-format',
+    '.gitignore': '.gitignore',
+}
+
+
+for name, path in files.items():
+    if not Path(path).exists():
+        copy_file(name, path)
