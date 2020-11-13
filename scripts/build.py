56186b2618662382ec3c72b09cfe7b717fabb46b
DaelonSuzuka <david@ldgelectronics.com>
2020-11-12 19:03:14 -0500

    Test implementation of depedency tree walker

--- scripts/build.py
+++ scripts/build.py
@@ -46,7 +46,7 @@ def xc8_cc(project, standard="C99"):
     return " ".join(command)
 
 
-def xc8(project):
+def xc8(project, files):
     """Builds the firmware using xc8 command in c89 mode."""
     command = ["xc8"]
     flag = command.append
@@ -81,17 +81,55 @@ def xc8(project):
     flag("-q")  # suppress the play-by-play of the compiler output
 
     # this MUST be last
-    # specify "*.c" as compilation targets
-    [flag(f.as_posix()) for f in Path(project.src_dir).rglob("*.c")]
+    [flag(f) for f in files]
 
     return " ".join(command)
 
+def scan_deps(files):
+    deps = {f: [] for f in files}
+
+    def strip_name(name):
+        return name.split('/')[-1][:-2]
+
+    names = {strip_name(f): f for f in files}
+
+    for f in files:
+        with open(f) as file:
+            for line in file.readlines():
+                if line.startswith('#include "'):
+                    name = strip_name(line[len('#include "'): -2])
+                    deps[f].append(name)
+
+    # walk the dependency tree
+    def check_deps(file):
+        used = [strip_name(file)]
+        # print(file, deps[file])
+        for dep in deps[file]:
+            used.append(dep)
+            if dep != strip_name(file) and dep in names:
+                used.extend(check_deps(names[dep]))
+        # print(used)
+        return used
+
+    used_files = check_deps('src/main.c')
+
+    # special cases
+    if 'uart' in used_files:
+        used_files.append('uart1')
+        used_files.append('uart2')
+
+    return [names[f] for f in set(used_files) if f in names]
+
 
 def main(project):
+    files = [f.as_posix() for f in Path(project.src_dir).rglob("*.c")]
+    
+    deps = scan_deps(files)
+
     if project.compiler == 'xc8-cc':
         command = xc8_cc(project)
     else:
-        command = xc8(project)
+        command = xc8(project, deps)
 
     try:
         result = os.system(command)
