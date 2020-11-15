b7a900c1e71796311e5dd243b58009048b2671bf
daelon <daelonsuzuka@gmail.com>
2020-11-15 17:04:52 -0500

    Improved install script

--- install.py
+++ install.py
@@ -9,32 +9,38 @@ import argparse
 
 
 templates_folder = Path(Path(__file__).absolute().parent, 'templates')
-template_files = json.load(open(Path(templates_folder, 'template.json')))
+template = Path(templates_folder, 'judi_example')
+template_files = [x for x in template.rglob('*') if not x.is_dir()]
 
 
-def copy_file(filename, dest):
+def copy_file(src):
+    dest = Path(src).relative_to(template)
+    
     if d := os.path.dirname(dest):
         os.makedirs(d, exist_ok=True)
-    shutil.copyfile(Path(templates_folder, filename), Path(dest))
+
+    shutil.copyfile(Path(templates_folder, src), dest)
 
 
 def install():
-    for name, path in template_files.items():
-        if not Path(path).exists():
-            copy_file(name, path)
+    for f in template_files:
+        if not Path(f).relative_to(template).exists():
+            copy_file(f)
 
 
-def uninstall():
-    for name, path in template_files.items():
-        os.remove(path)
+# def uninstall():
+#     for name, path in template_files.items():
+#         os.remove(path)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--uninstall", action="store_true")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--uninstall", action="store_true")
+    # args = parser.parse_args()
 
-    if args.uninstall:
-        uninstall()
-    else:
-        install()
+    # if args.uninstall:
+    #     uninstall()
+    # else:
+        # install()
+        
+    install()
