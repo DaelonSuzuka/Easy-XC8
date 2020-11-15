0f2a34df6739f7bcf03f82c5df846e2941e9f8db
daelon <daelonsuzuka@gmail.com>
2020-11-15 02:33:21 -0500

    Fixed platform agnosticism issues

--- cogscripts/cogutils/gperf.py
+++ cogscripts/cogutils/gperf.py
@@ -100,8 +100,8 @@ class Gperf:
 
     def run(self):
         self.create_temp_file()
-        result = check_output(self.command(file='strings.gperf')).decode()
-        result = result.split('\r\n')
+        result = check_output(self.command(file='strings.gperf'), shell=True).decode()
+        result = result.split('\n')
         self.remove_temp_file()
 
         if self.remove_line_directives:
