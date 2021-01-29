8fa72a6d3084cb528dbe5ab79092ed9c3941a88d
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-01-29 15:06:54 -0500

    Made project file parser more robust

--- scripts/project.py
+++ scripts/project.py
@@ -10,6 +10,11 @@ def get_commit_hash():
 
 
 def fix_project(proj):
+    if 'build_settings' in proj:
+        for key, value in proj['build_settings'].items():
+            proj[key] = value
+        proj.pop('build_settings')
+
     # mandatory
     if 'name' not in proj:
         raise Exception('Error: must define name in project.yaml')
@@ -24,24 +29,37 @@ def fix_project(proj):
     set_default('build_dir', 'build')
 
     # fix dev and release environments
+    pop_list = set()
     def fix_env(env):
         def inherit(key, default=None):
+            pop_list.add(key)
             if key not in env:
                 if key in proj:
                     env[key] = proj[key]
-                    proj.pop(key)
                 else:
                     env[key] = default
             if env[key] is None:
                 env[key] = default
 
-        inherit('processor', 'NO_PROCESSOR_SELECTED')
+        inherit('processor', None)
+        if env['processor'] is None:
+            raise Exception('Error: must define processor in project.yaml')
+
         inherit('toolchain_options', [])
         inherit('defines', [])
         inherit('force_include', [])
 
+    # make sure the environment definitions exist
+    set_default('development', {})
+    set_default('release', {})
+
     fix_env(proj['development'])
     fix_env(proj['release'])
+
+    # remove variables aren't inside one of the environments
+    for key in pop_list:
+        if key in proj:
+            proj.pop(key)
 
     return proj
 
@@ -50,7 +68,7 @@ def load_project():
     project = yaml.full_load(open("project.yaml").read())
     project = fix_project(project)
     project['git_hash'] = get_commit_hash()
-    
+    project['hexname'] = f"{project['build_dir']}/{project['name']}.hex"
     return DotMap(project)
 
 
