488a9647a114056ff609bb17f0105214942a2990
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-01-29 14:19:52 -0500

    Improved support for different project.yaml configurations

--- scripts/load_vars.py
+++ scripts/load_vars.py
@@ -4,13 +4,29 @@ import sys
 
 # ------------------------------------------------------------------------------
 
+# the name of the variable we're looking for
+key = sys.argv[1]
+
+# look for the variable in project.yaml
+# this is a fairly gross hack to not to have to import yaml
 if len(sys.argv):
     with open("project.yaml") as f:
         project = f.readlines()
 
         for line in project:
-            if sys.argv[1] + ":" in line:
+            if line.startswith(key + ":"):
                 print(line.split(":")[1].strip())
                 sys.exit(0)
 
+# if we get here, then the variable wasn't in project.yaml
+optional_vars = {
+    'src_dir': 'src',
+    'build_dir': 'build',
+    'obj_dir': 'obj',
+}
+if key in optional_vars:
+    print(optional_vars[key])
+    sys.exit(0)
+
+# if we get here, it wasn't in optional_vars either
 sys.exit(1)
--- scripts/project.py
+++ scripts/project.py
@@ -9,7 +9,51 @@ def get_commit_hash():
     return output.decode().replace('\n', '').replace('-', ':')
 
 
+def fix_project(proj):
+    # mandatory
+    if 'name' not in proj:
+        raise Exception('Error: must define name in project.yaml')
+
+    # optional with default values
+    def set_default(key, value):
+        if key not in proj:
+            proj[key] = value
+
+    set_default('src_dir', 'src')
+    set_default('obj_dir', 'obj')
+    set_default('build_dir', 'build')
+
+    # fix dev and release environments
+    def fix_env(env):
+        def inherit(key, default=None):
+            if key not in env:
+                if key in proj:
+                    env[key] = proj[key]
+                    proj.pop(key)
+                else:
+                    env[key] = default
+            if env[key] is None:
+                env[key] = default
+
+        inherit('processor', 'NO_PROCESSOR_SELECTED')
+        inherit('toolchain_options', [])
+        inherit('defines', [])
+        inherit('force_include', [])
+
+    fix_env(proj['development'])
+    fix_env(proj['release'])
+
+    return proj
+
+
 def load_project():
-    project = DotMap(yaml.full_load(open("project.yaml").read()))
-    project.git_hash = get_commit_hash()
-    return project
+    project = yaml.full_load(open("project.yaml").read())
+    project = fix_project(project)
+    project['git_hash'] = get_commit_hash()
+    
+    return DotMap(project)
+
+
+if __name__ == "__main__":
+    project = load_project()
+    print(project)
