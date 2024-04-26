11d9a41e3afd55954983995d4823d8241b053e4e
DaelonSuzuka <daelonsuzuka@gmail.com>
2024-04-26 19:59:04 +0000

    Remove system info from template

--- templates/judi_example/src/system.c
+++ templates/judi_example/src/system.c
@@ -18,27 +18,6 @@
 #include "usb/messages.h"
 
 /* ************************************************************************** */
-/*  System information
-
-    Various information about the system, made available at runtime.
-*/
-
-#define xstr(s) str(s)
-#define str(s) #s
-
-// product name
-const char productName[] = xstr(__PRODUCT_NAME__);
-
-// product software version
-const char productVersion[] = xstr(__PRODUCT_VERSION__);
-
-// compilation information
-const uint16_t xc8Version = __XC8_VERSION;
-const char processorModel[] = xstr(__PROCESSOR__);
-const char compileDate[] = __DATE__;
-const char compileTime[] = __TIME__;
-
-/* ************************************************************************** */
 
 // Set up the timer for the button isr
 static void button_isr_init(void) {
--- templates/judi_example/src/system.h
+++ templates/judi_example/src/system.h
@@ -4,24 +4,6 @@
 #include <stdint.h>
 
 /* ************************************************************************** */
-/*  System information
-
-    Various information about the system, made available at runtime.
-*/
-
-// product name
-extern const char productName[];
-
-// product software version
-extern const char productVersion[];
-
-// compilation information
-extern const uint16_t xc8Version;
-extern const char processorModel[];
-extern const char compileDate[];
-extern const char compileTime[];
-
-/* ************************************************************************** */
 
 // initialize the system
 extern void startup(void);
