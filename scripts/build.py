cc3c18503ab9ab4aa89202d30f602f75e219ed97
David Kincaid <daelonsuzuka@gmail.com>
2025-12-01 11:00:04 -0500

    Switch back to old compiler

--- scripts/build.py
+++ scripts/build.py
@@ -13,8 +13,8 @@ def compile():
     env = project.development
     sources = [f.as_posix() for f in Path(project.src_dir).rglob('*.c')]
 
-    # command = Xc8(project, env, sources)
-    command = Xc8CC(project, env, sources)
+    command = Xc8(project, env, sources)
+    # command = Xc8CC(project, env, sources)
     result = command.run()
 
     # hexfile = f'{project.build_dir}/{project.name}.hex'
