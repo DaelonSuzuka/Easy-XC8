8192f3acd676c1641369763affc17946a67f04c0
DaelonSuzuka <daelonsuzuka@gmail.com>
2025-12-03 20:18:44 -0500

    Update load_vars utility script

--- scripts/load_vars.py
+++ scripts/load_vars.py
@@ -2,31 +2,40 @@
 
 import sys
 
+from project import load_project
+
 # ------------------------------------------------------------------------------
 
-# the name of the variable we're looking for
-key = sys.argv[1]
 
-# look for the variable in project.yaml
-# this is a fairly gross hack to not to have to import yaml
-if len(sys.argv):
-    with open("project.yaml") as f:
-        project = f.readlines()
+def walk_project(project: dict, parts: list[str]):
+    if len(parts) == 1:
+        return project.get(parts[0], None)
 
-        for line in project:
-            if line.startswith(key + ":"):
-                print(line.split(":")[1].strip())
-                sys.exit(0)
+    return walk_project(project[parts[0]], parts[1:])
 
-# if we get here, then the variable wasn't in project.yaml
-optional_vars = {
-    'src_dir': 'src',
-    'build_dir': 'build',
-    'obj_dir': 'obj',
-}
-if key in optional_vars:
-    print(optional_vars[key])
-    sys.exit(0)
 
-# if we get here, it wasn't in optional_vars either
-sys.exit(1)
+def main(key: str):
+    project: dict = load_project()
+
+    parts = key.split('.')
+
+    if len(parts) == 1:
+        if key in project:
+            print(project[key])
+            sys.exit(0)
+    else:
+        result = walk_project(project, parts)
+        if result is not None:
+            print(result)
+            sys.exit(0)
+
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    # the name of the desired variable is the first argument
+    # e.g. name, src_dir, build_dir
+    # compound variables are separated by a dot, e.g. release.compiler
+    key = sys.argv[1]
+
+    main(key)
